211e858a3bf63e196c8481a368efd644c9bb33f5
Joellensilva <joellen.absilva@gmail.com>
2024-07-14 00:18:36 -0300

    consultando/listando itens e cadastrando manualmente

--- src/CentroDistribuicao.java
+++ src/CentroDistribuicao.java
@@ -1,8 +1,13 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CentroDistribuicao {
     private String nome;
+    private int id;
     private List<Roupa> roupas;
     private List<ProdutoHigiene> produtosHigiene;
     private List<Alimento> alimentos;
@@ -10,6 +15,14 @@ public class CentroDistribuicao {
 
     public CentroDistribuicao(String nome) {
         this.nome = nome;
+        this.roupas = new ArrayList<>();
+        this.produtosHigiene = new ArrayList<>();
+        this.alimentos = new ArrayList<>();
+    }
+
+    public CentroDistribuicao(int id) {
+        // this.nome = nome;
+        this.id = id;
         this.roupas = new ArrayList<>();
         this.produtosHigiene = new ArrayList<>();
         this.alimentos = new ArrayList<>();
@@ -50,18 +63,63 @@ public class CentroDistribuicao {
     }
 
     public void listarItens() {
-        System.out.println("Centro de Distribuição: " + nome);
-        System.out.println("Roupas:");
+        System.out.println("\nRoupas:");
         for (Roupa roupa : roupas) {
             roupa.exibirDetalhes();
         }
-        System.out.println("Produtos de Higiene:");
+        System.out.println("\nProdutos de Higiene:");
         for (ProdutoHigiene produto : produtosHigiene) {
             produto.exibirDetalhes();
         }
-        System.out.println("Alimentos:");
+        System.out.println("\nAlimentos:");
         for (Alimento alimento : alimentos) {
             alimento.exibirDetalhes();
         }
+    }
+
+    public void consultarItens() {
+        String sql = "select * from itens " +
+                "left join alimentos a on id = a.item_id " +
+                "left join roupas r on id = r.item_id " +
+                "where centro_id = ?";
+
+        try (Connection conn = DatabaseConnection.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            // Define o valor do parâmetro na consulta SQL
+            pstmt.setInt(1, id);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                // Processa os resultados da consulta
+                while (rs.next()) {
+                    String descricao = rs.getString("descricao");
+                    int tipo = rs.getInt("tipo_id");
+                    int quantidade = rs.getInt("quantidade");
+                    switch (tipo) {
+                        case 1:
+                            String unidadeDeMedida = rs.getString("unidade_medida");
+                            String validade = rs.getDate("validade").toString();
+                            Alimento alimento = new Alimento(descricao, quantidade, unidadeDeMedida, validade, id);
+                            adicionarAlimento(alimento);
+                            break;
+                        case 2:
+                            ProdutoHigiene produtoHigiene = new ProdutoHigiene(descricao, quantidade, id);
+                            adicionarProdutoHigiene(produtoHigiene);
+                            break;
+                        case 3:
+                            String genero = rs.getString("genero");
+                            String tamanho = rs.getString("tamanho");
+                            Roupa roupa = new Roupa(descricao, genero, tamanho, quantidade, id);
+                            adicionarRoupa(roupa);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        listarItens();
     }
 }
